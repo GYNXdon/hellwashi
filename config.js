f8721a2af7fe34758952baf551aaa58af84a8516
GYNXdon <115105102+GYNXdon@users.noreply.github.com>
2023-04-16 17:32:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export default {
   baseUrl: 'https://api.kucoin.com', // or 'https://api.kucoin.com' for the production API
   apiAuth: {
-    key: '64385f48e7c6660001acf87a', // KC-API-KEY
-    secret: 'bb8d5166-aded-4b56-b33a-90fdabfc055d', // API-Secret
-    passphrase: '203246', // KC-API-PASSPHRASE
+    key: '', // KC-API-KEY
+    secret: '', // API-Secret
+    passphrase: '', // KC-API-PASSPHRASE
   },
   authVersion: 2, // KC-API-KEY-VERSION. Notice: for v2 API-KEY, not required for v1 version.
 }
