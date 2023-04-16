0fdcfa0a04a10ac51e0bcfcf4c321072f62f5063
GYNXdon <115105102+GYNXdon@users.noreply.github.com>
2023-04-16 16:53:11 +0100

    Update config.js
    change to Kucoin api layout

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 export default {
-    apiKey: "64385f48e7c6660001acf87a",
-    apiSecret: "bb8d5166-aded-4b56-b33a-90fdabfc055d",
-    passPhrase: "203246"
-  }
+  baseUrl: 'https://api.kucoin.com', // or 'https://api.kucoin.com' for the production API
+  apiAuth: {
+    key: '64385f48e7c6660001acf87a', // KC-API-KEY
+    secret: 'bb8d5166-aded-4b56-b33a-90fdabfc055d', // API-Secret
+    passphrase: '203246', // KC-API-PASSPHRASE
+  },
+  authVersion: 2, // KC-API-KEY-VERSION. Notice: for v2 API-KEY, not required for v1 version.
+}
